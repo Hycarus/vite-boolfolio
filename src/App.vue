6d3e7757f7ead6ab157037c28c28a9afbe2b06ee
Hycarus <mirkomarasco08@gmail.com>
2024-01-22 18:23:19 +0100

    created scaffholding

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <AppHeader></AppHeader>
+  <router-view></router-view>
+</template>
+
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use './assets/styles/partials/variables' as *;
+</style>
