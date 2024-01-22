d9f2500a6b20cc9971a6b4bad8cb6954295d3e18
Hycarus <mirkomarasco08@gmail.com>
2024-01-22 18:26:28 +0100

    added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item" v-for="item in menu">
+                        <router-link class="nav-link active" :to="{ name: item.name }">{{ item.label }}</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    components: {
+
+    },
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'Home',
+                    name: 'home',
+                },
+                {
+                    label: 'Projects',
+                    name: 'projects',
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
