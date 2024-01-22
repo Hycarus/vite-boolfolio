88793ba84ba7ec8fcacac1da142866954497b66b
Hycarus <mirkomarasco08@gmail.com>
2024-01-22 18:24:39 +0100

    added route

---
+++ src/router.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppHome from './pages/AppHome.vue';
+import AppProjects from './pages/AppProjects.vue';
+import SingleProject from './pages/SingleProject.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: AppHome,
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: AppProjects,
+        },
+        {
+            path: '/projects/:slug',
+            name: 'project:slug',
+            component: SingleProject,
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFound
+        }
+    ]
+});
+
+export default router;
