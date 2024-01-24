4e7260452011ec82774a8a3076051feb76dda1e7
Hycarus <mirkomarasco08@gmail.com>
2024-01-24 11:20:45 +0100

    added category

--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -1,5 +1,12 @@
 
 <template>
+    <div>
+        <select v-model="selectedCategory" @change="selectedProjectsByCategory()" class=" my-5" id="category">
+            <option value="all">All Category</option>
+            <option v-for="category in store.categories" :key="category.id" :value="category.id">{{ category.name }}
+            </option>
+        </select>
+    </div>
     <div class="container ">
         <h1 class="my-5 text-white">Projects List</h1>
         <div class="row">
@@ -27,7 +34,8 @@ export default {
             store,
             currentPage: 1,
             lastPage: null,
-            total: 0
+            total: 0,
+            selectedCategory: 'all'
         }
     },
     methods: {
@@ -38,6 +46,13 @@ export default {
                 this.total = res.data.results.total
                 this.currentPage = res.data.results.current_page
             })
+        },
+        getAllCategories() {
+            if (this.store.categories.length < 1) {
+                axios.get(this.store.apiUrl + "categories").then((res) => {
+                    this.store.categories = res.data.results
+                })
+            }
         },
         previousPage() {
             if (this.currentPage > 1) {
@@ -50,10 +65,25 @@ export default {
                 this.currentPage++
                 this.getAllProjects()
             }
-        }
+        },
+        selectedProjectsByCategory() {
+            if (this.selectedCategory == "all") {
+                this.getAllProjects();
+            } else {
+                axios
+                    .get(store.apiUrl + "projects", {
+                        params: { category: this.selectedCategory },
+                    })
+                    .then((response) => {
+                        console.log(response.data.results);
+                        this.store.projects = response.data.results
+                    });
+            }
+        },
     },
     mounted() {
         this.getAllProjects()
+        this.getAllCategories()
     }
 
 }
--- src/store.js
+++ src/store.js
@@ -3,4 +3,5 @@ export const store = reactive({
     apiUrl: 'http://localhost:8000/api/',
     imgPath: 'http://localhost:8000/storage/',
     projects: [],
+    categories: [],
 })
